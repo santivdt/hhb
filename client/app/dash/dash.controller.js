dcb76d3a5d332d79bae3dcc021ae49bef9273572
Santi van den Toorn <santi.vdt@gmail.com>
2015-12-28 17:22:08 +0100

    Start dashboard

--- client/app/dash/dash.controller.js
+++ client/app/dash/dash.controller.js
@@ -1,6 +1,40 @@
 'use strict';
 
 angular.module('hhbApp')
-  .controller('DashCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('DashCtrl', function ($scope, $http) {
+
+
+
+    // when landing on the page, get all entries and show them, then execute calculation methods
+    $http.get('/api/entries')
+      .success(function(data) {
+        $scope.entries = data;
+        console.log(data);
+      })
+      .error(function(data) {
+        console.log('Error: ' + data);
+      });
+
+
+
+    // get total amount of expenses
+    // TODO make it so that it only calculates for expense not income, also counts income meh
+    $scope.totalExpense = function(){
+      var total = 0;
+      for (var j = 0; j < $scope.entries.length; j++) {
+        if ($scope.entries[j].flow === 'income') {
+          for (var i = 0; i < $scope.entries.length; i++) {
+            total += $scope.entries[i].amount;
+          }
+          $scope.total = total;
+        }
+      }
+
+      for ( j = 0; j < $scope.entries.length; j++) {
+        if ($scope.entries[j].flow === 'expense') {
+          console.log('ja');
+        }
+      }
+    };
+
   });
