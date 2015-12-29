5394a9f3e27f07765ab2010da7fc6bcfcd4b2cf6
Santi van den Toorn <santi.vdt@gmail.com>
2015-12-29 18:38:48 +0100

    Adjust categories API

--- server/api/category/category.model.js
+++ server/api/category/category.model.js
@@ -3,9 +3,7 @@
 var mongoose = require('bluebird').promisifyAll(require('mongoose'));
 
 var CategorySchema = new mongoose.Schema({
-  name: String,
-  info: String,
-  active: Boolean
+  category: String
 });
 
 export default mongoose.model('Category', CategorySchema);
