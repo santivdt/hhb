d673a596be2c330a35e8020972800dd2c0174307
daphnes <daphne@tamtam.nl>
2016-01-06 21:34:21 +0100

    commit indent

--- client/app/dash/dash.js
+++ client/app/dash/dash.js
@@ -1,19 +1,19 @@
 'use strict';
 
 angular.module('hhbApp')
-  .config(function ($stateProvider) {
-    $stateProvider
-      .state('dash', {
-        url: '/dash',
-        resolve: {
-              categories:  function($http){
-                  return $http.get('/api/categories');
-              },
-                entries:  function($http){
-                return $http.get('/api/entries');
-            }
-          },
-        templateUrl: 'app/dash/dash.html',
-        controller: 'DashCtrl'
-      });
-  });
+	.config(function ($stateProvider) {
+		$stateProvider
+			.state('dash', {
+				url: '/dash',
+				resolve: {
+					categories: function ($http) {
+						return $http.get('/api/categories');
+					},
+					entries: function ($http) {
+						return $http.get('/api/entries');
+					}
+				},
+				templateUrl: 'app/dash/dash.html',
+				controller: 'DashCtrl'
+			});
+	});
