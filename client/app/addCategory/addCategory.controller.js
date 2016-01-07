789c4170a9649f227276b62c46e4e7d21e43778d
Santi van den Toorn <santi.vdt@gmail.com>
2016-01-07 14:34:59 +0100

    Start complex category logic

--- client/app/addCategory/addCategory.controller.js
+++ client/app/addCategory/addCategory.controller.js
@@ -7,6 +7,11 @@ angular.module('hhbApp')
       $scope.categories = categories.data;
       $scope.entries = entries.data;
 
+      //modal data
+      $scope.items = ['item1', 'item2', 'item3'];
+      $scope.animationsEnabled = true;
+
+
       // when submitting the category, send the input to the node API
     $scope.addCategory = function() {
       console.log('addCat');
@@ -47,6 +52,7 @@ angular.module('hhbApp')
               });
       };
 
+
       // calculate total uses of each category and add them to category array
       $scope.calculateTotals = function () {
           $scope.totals = [];
@@ -58,4 +64,70 @@ angular.module('hhbApp')
           }
 
       };
+
+      // delete all entries in specific category
+      $scope.deleteEntriesByCategory = function(categoryToDelete){
+          console.log('function called, deleting entries in ' + categoryToDelete + ' and category');
+
+          //get id of category to delete
+          for (var j = 0; j<$scope.categories.length; j++){
+              if ($scope.categories[j].title == categoryToDelete) {
+                  var id = $scope.categories[j]._id;
+              }
+          }
+
+          //delete all the entries in the category
+          for (var i = 0; i<$scope.entries.length; i++){
+                  if ($scope.entries[i].category == categoryToDelete){
+                      $http.delete('/api/entries/' + $scope.entries[i]._id)
+                          .success(function(data) {
+                              console.log(' All entries deleted from ' + categoryToDelete + ' succesfully');
+                          }).error(function(data) {
+                              console.log('Error: ' + data);
+                          });
+                  }
+          }
+
+          //delete the category
+          $http.delete('/api/categories/' + id)
+              .success(function(data) {
+                  console.log(categoryToDelete + ' is sucessfully deleted. Hopefully you really wanted this ;)');
+              })
+              .error(function(data) {
+                  console.log('Error: ' + data);
+              });
+      }
+
+
+      //function to assign all entries of a cat to a different cat
+      $scope.assignCat = function(oldCategory, newCategory) {
+          for (var i = 0; i < $scope.entries.length; i++){
+              if ($scope.entries[i].category === oldCategory){
+                  $scope.entries[i].category = newCategory;
+              }
+          }
+
+          // if category has to be deleted get id
+          if ($scope.deleteOldCat) {
+              for (var i = 0; i < $scope.categories.length; i++){
+                  if($scope.categories[i].title === oldCategory){
+                      var id = $scope.categories[i]._id;
+                      console.log(id);
+                  }
+              }
+
+          //delete old category
+          $http.delete('/api/entries/' + id)
+              .success(function(data) {
+                  console.log(oldCategory + ' deleted succesfully');
+              }).error(function(data) {
+              console.log('Error: ' + data);
+                });
+          }
+
+
+
+      }
+
   });
+
